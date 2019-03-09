7dd6014808baa91e842b87f6a88c9096ff2ba5ba
Thread7 <Thread7@gmail.com>
2019-03-09 13:31:14 -0500

    Create alt.js
    test

---
+++ js/alt.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('.submitBtn').on('click', function() {
+
+   console.log("Clicked!");
+
+  })
+
+$('.submitBtn').click();
+$('.submitBtn').trigger('click')
+
+
+
+});
